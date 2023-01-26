eb31c9559d2dd70a8c486710c8e71ceade9e956d
liqowomo <123171512+liqowomo@users.noreply.github.com>
2023-01-26 12:17:16 +0000

    🍲: rust-syscoms - Rust System Commands

--- lq/inst.sh
+++ lq/inst.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo " 
-Running the following command 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-source "$HOME/.profile"
-source "$HOME/.cargo/env"
-"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-source "$HOME/.profile"
+echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/codespace/.profile
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/codespace/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo apt-get install build-essential
+brew install gcc
+echo "Now run - fish shell - source ~/.profile"
+echo "Now run - Fish shell - source $HOME/.cargo.env" 
+source "/home/codespace/.profile"
 source "$HOME/.cargo/env"
